49817e5ac132b06274c820f1a76ebb02ea51805e
awkumar <30897745+awkumar@users.noreply.github.com>
2021-12-15 00:30:27 +0530

    Initial first dev Code

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name_prefix" {
+default = "rg"
+description = "Prefix of the resource group name that's combined with a random ID so name isunique in your Azure subscription."
+}
+variable "resource_group_location" {
+default = "eastus"
+description = "Location of the resource group."
+}
